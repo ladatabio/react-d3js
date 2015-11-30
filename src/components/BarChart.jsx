fdc14d2edbeb54bc53afdebda9c980ed111dfc90
Guillaume Mousnier <mousnier.guillaume@gmail.com>
2015-11-30 17:56:57 +0100

    Create a new component BarChart. TODO: Finish BarChart component and example

---
+++ src/components/BarChart.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class BarChart extends React.Component {
+
+    constructor(){
+        super();
+    }
+
+    render(){
+        return (<div>Hello</div>);
+    }
+
+}
+
+export default BarChart;
