542c86522607354f15cbfdc2be8fcb1fe2a30a9d
Guillaume Mousnier <mousnier.guillaume@gmail.com>
2016-03-31 09:27:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import Rectangles from './components/Rectangles.jsx';
-import Circles from './components/Circles.jsx';
-import Lines from './components/Lines.jsx';
-import Texts from './components/Texts.jsx';
-import Paths from './components/Paths.jsx';
-import Ellipses from './components/Ellipses.jsx';
-import Animate from './components/Animate.jsx';
-import SVGContainer from './components/SVGContainer.jsx';
-import XAxis from './components/XAxis.jsx';
-import YAxis from './components/YAxis.jsx';
-import BarChart from './components/BarChart.jsx';
-import PieChart from './components/PieChart.jsx';
-import LineChart from './components/LineChart.jsx';
+import Rectangles from './components/Rectangles';
+import Circles from './components/Circles';
+import Lines from './components/Lines';
+import Texts from './components/Texts';
+import Paths from './components/Paths';
+import Ellipses from './components/Ellipses';
+import Animate from './components/Animate';
+import SVGContainer from './components/SVGContainer';
+import XAxis from './components/XAxis';
+import YAxis from './components/YAxis';
+import BarChart from './components/BarChart';
+import PieChart from './components/PieChart';
+import LineChart from './components/LineChart';
 
 
 export { Rectangles, Circles, Lines, Texts, Paths, Ellipses, Animate, SVGContainer, XAxis, YAxis, BarChart, PieChart, LineChart };
